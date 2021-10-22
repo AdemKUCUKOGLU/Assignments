67f8073918cf3c43c3779bad50ebbb04132d33b7
AdemKUCUKOGLU <87221486+AdemKUCUKOGLU@users.noreply.github.com>
2021-10-22 22:35:14 +0300

    Largest-Number added.

---
+++ Largest-number.py
@@ -0,0 +1,8 @@
+num1 = int(input ("please enter 5 numbers: \nnumber 1 : "))
+num2 = int(input ("number 2 : "))
+num3 = int(input ("number 3 : "))
+num4 = int(input ("number 4 : "))
+num5 = int(input ("number 5 : "))
+numbers = [num1, num2, num3, num4, num5]
+numbers.sort()
+print("The largest number is : ", numbers[4]) 
